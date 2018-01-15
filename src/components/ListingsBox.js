af99a7d832bfef145feddc2b003e057731d76e42
Kynan <k.song@blueyonder.co.uk>
2018-01-15 22:34:06 +0000

    Can submit new listings. Homework Complete.

--- src/components/ListingsBox.js
+++ src/components/ListingsBox.js
@@ -11,14 +11,22 @@ class ListingsBox extends React.Component {
         {id: 1, movieName: 'The Disaster Artist', ageRating: '18', movieTime: '4:30'},
         {id: 2, movieName: 'Big Trouble in Little China', ageRating: '12', movieTime: '15:25'}
       ]
-    }
+    };
+
+    this.handleListingSubmit = this.handleListingSubmit.bind(this);
+
   };
+
+  handleListingSubmit(listing) {
+    const newListings = this.state.listings.concat([listing]);
+    this.setState({listings: newListings})
+  }
 
   render() {
     return(
       <div className='listings-box'>
         <h1>Movies</h1>
-        <ListingsForm />
+        <ListingsForm handleListingSubmit={this.handleListingSubmit}/>
         <ListingsList listings={this.state.listings}/>
       </div>
     );
--- src/components/ListingsForm.js
+++ src/components/ListingsForm.js
@@ -1,22 +1,71 @@
 import React from 'react';
 
 class ListingsForm extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      movieName: '',
+      ageRating: '',
+      movieTime: ''
+    };
+
+    this.handleMovieNameChange = this.handleMovieNameChange.bind(this);
+    this.handleAgeRatingChange = this.handleAgeRatingChange.bind(this);
+    this.handleMovieTimeChange = this.handleMovieTimeChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  };
+
+  handleMovieNameChange(event) {
+    this.setState({movieName: event.target.value});
+  };
+
+  handleAgeRatingChange(event) {
+    this.setState({ageRating: event.target.value});
+  };
+
+  handleMovieTimeChange(event) {
+    this.setState({movieTime: event.target.value});
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const movieName = this.state.movieName.trim();
+    const ageRating = this.state.ageRating.trim();
+    const movieTime = this.state.movieTime.trim();
+
+    if(!movieName || !ageRating || !movieTime) return;
+
+    this.props.handleListingSubmit({id: Date.now(), movieName: movieName, ageRating: ageRating, movieTime: movieTime});
+
+    this.setState({movieName: '', ageRating: '', movieTime: ''})
+  }
+
   render() {
     return(
-      <form className="listings-form">
+      <form className="listings-form" onSubmit={this.handleSubmit}>
         <input type="text"
-          placeholder="Movie Name"/>
+          placeholder="Movie Name"
+          value={this.state.movieName}
+          onChange={this.handleMovieNameChange}/>
         <input type="text"
-          placeholder="Age Rating"/>
+          placeholder="Age Rating"
+          value={this.state.ageRating}
+          onChange={this.handleAgeRatingChange}/>
         <input type="text"
-          placeholder="Show Time"/>
+          placeholder="Movie Time"
+          value={this.state.movieTime}
+          onChange={this.handleMovieTimeChange}/>
 
-        <input type="submit"/>
+        <input type="submit" value="Submit"/>
 
 
       </form>
-    )
-  }
-}
+    );
+  };
+};
 
 export default ListingsForm;
