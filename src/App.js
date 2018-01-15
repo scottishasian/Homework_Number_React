d9c11a79509aca2e5d6127fae434e84fa6685b4e
Kynan <k.song@blueyonder.co.uk>
2018-01-15 21:03:35 +0000

    Created basic files and listings box.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ListingsBox from './components/ListingsBox.js'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ListingsBox />
+
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ListingsBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ListingsBox extends React.Component {
+  // constructor(props){
+  //   super(props);
+  // };
+
+  render() {
+    return(
+      <div className='listings-box'>
+        <h1>Movies</h1>
+      </div>
+    );
+  }
+
+
+}
+
+export default ListingsBox;
